56cdd066fd21256b8b95ddf2350d9b2ba76e2039
Trần Lương Tiến Sĩ - 19522128 <91487292+tranluongtiensi@users.noreply.github.com>
2022-11-05 12:55:20 +0700

    DFDF

---
+++ Lab3/Exercise4/exercise4.1/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+
+const element = (
+<div>Green is the prime color of the world</div>
+);
+ReactDOM.render(element, document.getElementById('root'));
---
+++ Lab3/Exercise4/exercise4.2/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+const element = () => {
+  <button style={{margin: "10px 10px"}} type="primary"></button>
+}
+ReactDOM.render(element, document.getElementById('root'));
