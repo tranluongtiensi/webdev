a077a94b39304f8b6951e94bc532395dd5e2121a
Trần Lương Tiến Sĩ - 19522128 <91487292+tranluongtiensi@users.noreply.github.com>
2022-11-30 15:35:42 +0700

    update ex16

--- Lab4/Ex1/exercise1/src/App.js
+++ Lab4/Ex1/exercise1/src/App.js
@@ -1,15 +1,16 @@
-import Ex1_1 from './components/Ex1.1/Ex1_1';
+// import Ex1_1 from './components/Ex1.1/Ex1_1';
 // import Ex1_2 from './components/Ex1.2/Ex1_2';
 // import Ex1_3_1 from './components/Ex1.3/Ex1_3_1/Ex1_3_1';
 // import Ex1_3_2 from './components/Ex1.3/Ex1_3_2/Ex1_3_2';
 // import Ex13 from './components/Ex1.3/Ex1_3';
 // import Ex1_4 from './components/Ex1.4/Ex1_4';
 // import Ex15 from './components/Ex1.5/Ex1_5';
+import Ex16 from './components/Ex1.6/Ex1_6';
 
 function App() {
   return (
     <>
-      <Ex1_1/>
+      {/* <Ex1_1/> */}
       <hr/>
       {/* <Ex1_2/> */}
       {/* <Ex13/> */}
@@ -21,6 +22,7 @@ function App() {
       {/* <Ex1_4/> */}
       <hr/>
       {/* <Ex15/> */}
+      <Ex16/>
     </>
   );
 }
